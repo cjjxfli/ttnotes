189c79d5426e2cb39d60afa1dc3271b257c0842e
xfli <540331240@qq.com>
2018-05-25 23:23:07 +0800

    修改bug

--- trunk/Application/Common/Conf/config.php
+++ trunk/Application/Common/Conf/config.php
@@ -55,9 +55,9 @@ return array(
         'SMTP_HOST'   => 'smtp.exmail.qq.com', //SMTP服务器
         'SMTP_PORT'   => '465', //SMTP服务器端口
         'SMTP_USER'   => '540331240@toubao.mtaotop.com', //SMTP服务器用户名
-        'SMTP_PASS'   => 'Xfli181018', //SMTP服务器密码
+        'SMTP_PASS'   => '123456', //SMTP服务器密码
         'FROM_EMAIL'  => '540331240@toubao.mtaotop.com', //发件人EMAIL
-        'FROM_NAME'   => 'toubao', //发件人名称
+        'FROM_NAME'   => 'ttnotes', //发件人名称
         'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
         'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
     ),
